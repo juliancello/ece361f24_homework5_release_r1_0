209b25b86fbf51c01a329e229b0b9b98c4d8cc58
Julian Kosanovic <jmkosanovic@gmail.com>
2024-12-02 23:24:24 -0800

    Added header similar to BinaryTree.c

--- starters/karumanchi_bst/BinarySearchTree.c
+++ starters/karumanchi_bst/BinarySearchTree.c
@@ -1,3 +1,12 @@
+/*Copyright (c) 2016 CareerMonk Publications and others.
+#E-Mail            : info@careermonk.com
+#Creation Date     : Unknown
+#Created by        : Narasimha Karumanchi
+#Book Title        : Data Structures And Algorithms Made Easy
+#Warranty          : This software is provided "as is" without any
+#                    warranty; without even the implied warranty of
+#                    merchantability or fitness for a particular purpose.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 

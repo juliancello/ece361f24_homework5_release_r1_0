fdc3dce1b9be64cff313b23b785f7779fb76c5f8
Julian Kosanovic <jmkosanovic@gmail.com>
2024-12-02 23:25:48 -0800

    Created binary search tree abstract data type source and header files

---
+++ starters/karumanchi_bst/BST_ADT.c
@@ -0,0 +1,18 @@
+/**
+* @file BST_ADT.c
+*
+* @version 1.0.0
+* @author Julian Kosanovic (jkosa2@pdx.edu)
+* @date   2-Dec-2024
+*
+* @brief
+* Binary search tree abstract data type
+*
+* @details
+* Revision History
+* ----------------
+
+*	1.0.0	(02-Dec-2024)	Initial version
+*
+* @note TODO
+*/
---
+++ starters/karumanchi_bst/BST_ADT.h
@@ -0,0 +1,26 @@
+/**
+* @file BST_ADT.h
+*
+* @version 1.0.0
+* @author Julian Kosanovic (jkosa2@pdx.edu)
+* @date   2-Dec-2024
+*
+* @brief
+* Binary search tree abstract data type header file.
+*
+* @details
+* Revision History
+* ----------------
+
+*	1.0.0	(02-Dec-2024)	Initial version
+*
+* @note TODO
+*/
+
+#ifndef BST_ADT_H
+#define BST_ADT_H
+
+#include "BinaryTree.h"
+#include "BinarySearchTree.h"
+
+#endif //BST_ADT_H
